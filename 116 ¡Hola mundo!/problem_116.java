dd853049c606a23486e579b88dee03e965815dee
Jason171096 <37201661+Jason171096@users.noreply.github.com>
2019-09-03 07:31:35 -0500

    Create problem_116.java

---
+++ 116 ¡Hola mundo!/problem_116.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class problem_116 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0; i<n; i++)
+            System.out.println("Hola mundo.");
+    }  
+}
